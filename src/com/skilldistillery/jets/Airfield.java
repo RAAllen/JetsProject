e39dd0f893825908ac065079df540d848e520a9e
Rebecca Allen <rebeccazarsky@gmail.com>
2018-03-10 11:57:27 -0700

    start work on airfield and jetapp classes

---
+++ src/com/skilldistillery/jets/Airfield.java
@@ -0,0 +1,18 @@
+package com.skilldistillery.jets;
+
+public class Airfield {
+	private Jet[] jets = new Jet[100];
+
+	public Airfield() {
+		
+	}
+
+	public Jet[] getJets() {
+		return jets;
+	}
+
+	public void setJets(Jet[] jets) {
+		this.jets = jets;
+	}
+	
+}
---
+++ src/com/skilldistillery/jets/JetsApplication.java
@@ -0,0 +1,23 @@
+package com.skilldistillery.jets;
+
+import java.util.Scanner;
+
+public class JetsApplication {
+	private Airfield airfield = new Airfield();
+	Scanner scanner = new Scanner(System.in);
+	
+	public JetsApplication() {
+	}
+	
+	public static void main(String[] args) {
+		run();
+	}
+	
+	private static void run() {
+		displayUserMenu();
+	}
+	
+	private static void displayUserMenu() {
+		
+	}
+}
